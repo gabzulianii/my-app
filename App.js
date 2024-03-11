6f1e97d69f511ff04ad837b790291aae7e601b80
gabzulianii <gabrielazuliani22@gmail.com>
2024-03-11 11:06:04 -0300

    Instalando suporte para Web

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>"BOM DIA PRINCESa POR FAVOR SENTE NA GLOCk"</Text>
       <StatusBar style="auto" />
     </View>
   );
